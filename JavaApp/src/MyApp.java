130a29f83392eea1307e52cc2762876c858c0a74
anuruddha.kulatunga <akimasao@gmail.com>
2021-03-17 00:12:18 +0900

    First commit

---
+++ JavaApp/src/MyApp.java
@@ -0,0 +1,10 @@
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		String[] projectNames = { "MDM", "Cockpit" };
+		for (int i = 0; i < projectNames.length; i++) {
+			System.out.println("Project name:" + projectNames[i]);
+		}
+	}
+}
