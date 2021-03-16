bbbee9ffd4b3633e4bed1f163b36f79eaf39d262
kula87 <akimasao@gmail.com>
2021-03-17 00:16:47 +0900

    Update MyApp.java

--- JavaApp/src/MyApp.java
+++ JavaApp/src/MyApp.java
@@ -1,10 +1,10 @@
-
-public class MyApp {
+public class JavaApp {
 
 	public static void main(String[] args) {
-		String[] projectNames = { "MDM", "Cockpit" };
-		for (int i = 0; i < projectNames.length; i++) {
-			System.out.println("Project name:" + projectNames[i]);
+		String[] projectNames = {"MDM","Cockpit"};
+		for (String projectName : projectNames) {
+			System.out.println("Project name:" + projectName);
 		}
 	}
+
 }
